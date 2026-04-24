952cbe255f6b852ab67cb48dbbba5d856988da9c
kezjo77 <25ug1bycs0541@bmsit.in>
2026-04-25 00:22:22 +0530

    changes saved

--- miniproj.py
+++ miniproj.py
@@ -1,26 +1,50 @@
-import random,msvcrt
+import random,time,msvcrt
 print("WELCOME TO THE ATM XYZ BANK")
 print("PLEASE PUT YOUR CARD IN")
-'''
 print(" 1.Check balance \n 2.Deposit \n 3.Withdraw")
 choice=int(input("Choose your option(1/2/3)"))
 card_balance=random.randint(100,10000000)
+def pinnum():
+    pin = ""
+    print("Enter PIN: \n", end="")
+    while True:
+        x = msvcrt.getch()
+        if x in (b'\x00', b'\xe0'):
+            msvcrt.getch()
+            continue
+        x = x.decode()
+        if x == '\r':
+            print()
+            break
+        elif x == '\x08':
+            if pin != "":
+                pin = pin[:-1]       
+                print('\b \b', end='',flush=True)
+        elif x.isdigit():
+            pin += x                 
+            print("*", end='',flush=True)
+        return pin
 def balance():
+    pinnum()
     for i in range(1,4):
         print("."*i,end='\r')
-        time.sleep(2)
+        time.sleep(1)
     print("YOUR BALANCE IS : Rs",card_balance)
     thankyou()
 def withdraw():
     amount=float(input("Withdraw Rs:" ))
+    pinnum()
     print("DO NOT TAKE YOUR CARD OUT DURING THE PROCESS")
     for i in range(1,4):
         print("."*i,end='\r')
-        time.sleep(2)
+        time.sleep(1)
+    global card_balance
+    card_balance -= amount
     print("YOUR CASH HAS BEEN WITHDRAWN.\nWould you like to see balance?(Y/N) ")
-    c=input()
+    c=input().upper()
     if c=='Y':
-        print("Balance is Rs",card_balance-amount)
+
+        print("Balance is Rs",card_balance)
         thankyou()
     else:
         thankyou()
@@ -28,10 +52,13 @@ def thankyou():
     print("Have a great day!")
 def deposit():
     deposit=float(input("How much do you want to deposit? Rs"))
+    pinnum()
     for i in range(1,4):
         print("."*i,end='\r')
-        time.sleep(2)
-    print("Your amount has been deposited. \nYour new balance is Rs",card_balance+deposit)
+        time.sleep(1)
+    global card_balance
+    card_balance += deposit
+    print("Your amount has been deposited. \nYour new balance is Rs",card_balance)
     thankyou()
 if choice==1:
     balance()
@@ -40,26 +67,4 @@ elif choice==2:
 elif choice==3:
     withdraw()
 else:
-    print("Invalid choice")'''
-
-
-def pinnum():
-    pin = ""
-    print("Enter PIN: ", end="")
-
-    while True:
-        x = msvcrt.getch().decode()   
-
-        if x == '\r':
-            break
-
-        elif x == '\x08':
-            if pin != "":
-                pin = pin[:-1]       
-                print('\b \b', end='')
-
-        elif x.isdigit():
-            pin += x                 
-            print("*", end='')
-
-pinnum()
+    print("Invalid choice")
